5c581e24880c0f3d1e2e60d44cd5be931fa9f0da
WORKGROUP\A_Korotkov <a_korotkov@step.ru>
2024-04-10 18:31:23 +0300

    commandsStart

--- homework-03/sender.py
+++ homework-03/sender.py
@@ -3,9 +3,30 @@ import requests
 name = input('Введите имя: ')
 while True:
     text = input('Введите сообщение: ')
-    response = requests.post('http://127.0.0.1:5000/send',
-                             json={
-                                 'name': name,
-                                 'text': text
-                             }
+    if text.startswith('/'):
+        text = text[1:]
+        text = text.rstrip()
+
+        if text == "help":
+            print("Список команд:\n/help\n/weather\n/anon\n/deanon")
+
+        elif text == "weather":
+            pass
+
+        elif text == "anon":
+            name = "Аноним"
+            print("Ваши сообщения теперь анонимны, для отмены операции введите /deanon")
+
+        elif text == "deanon":
+            name = input('Введите имя: ')
+
+        else:
+            print("Такой команды нет, список команд:\n/help\n/weather\n/anon\n/deanon")
+            
+    else:
+        response = requests.post('http://127.0.0.1:5000/send',
+                                json={
+                                'name': name,
+                                'text': text
+                                }
                             )
